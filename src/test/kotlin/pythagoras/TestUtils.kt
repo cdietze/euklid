cb053c3531dbd732406ab1b0b40f28d25c2b224c
Christoph Dietze <christoph.dietze@gmail.com>
2017-11-02 22:13:34 +0100

    Comment

--- src/test/kotlin/pythagoras/TestUtils.kt
+++ src/test/kotlin/pythagoras/TestUtils.kt
@@ -3,6 +3,7 @@ package pythagoras
 import kotlin.math.abs
 import kotlin.test.assertTrue
 
+/** Asserts that the difference between `expected` and `actual` is less than or equal to `delta`. */
 fun assertEquals(expected: Float, actual: Float, delta: Float, message: String? = null) {
     assertTrue(abs(expected - actual) <= delta, message)
 }
